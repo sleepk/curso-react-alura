ebc52a5a883482003748148e2ad8b04259f382a2
Julio Cesar Rodrigues De Souza <jcsouza@latam.stefanini.com>
2020-01-13 11:16:35 -0300

    Capitulo 5 - Finalizado projeto do primeiro curso react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component, Fragment } from 'react';
-import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
 import Tabela from './Tabela';
 import Form from './Formulario';
+import Header from './Header';
+import './App.css';
 
 class App extends Component {
   state = {
@@ -25,6 +27,7 @@ class App extends Component {
         nome: 'Bruno',
         livro: 'DevOps',
         preco: '100'
+
       }
     ]
   }
@@ -53,8 +56,11 @@ escutadorDeSubmit = autor => {
   render() {
     return (
       <Fragment>
+        <Header/>
+        <div className="container mb-10">
         <Tabela autores={this.state.autores} removeAutor={this.removeAutor} />
         <Form escutadorDeSubmit={this.escutadorDeSubmit}/>
+        </div>
       </Fragment>
     );
   }
--- src/Tabela.js
+++ src/Tabela.js
@@ -20,7 +20,7 @@ const TableBody = props => {
         <td>{linha.nome}</td>
         <td>{linha.livro}</td>
         <td>{linha.preco}</td>
-        <td><button onClick={() => { props.removeAutor(index) }}>Remover</button></td>
+        <td><button onClick={() => { props.removeAutor(index) }} className="waves-effect waves-light indigo lighten-2 btn">Remover</button></td>
       </tr>
     )
   })
@@ -39,7 +39,7 @@ class Tabela extends Component {
     console.log(autores);
 
     return (
-      <table>
+      <table className="centered highlight">
         <TableHead />
         <TableBody autores={autores} removeAutor={removeAutor} />
       </table>
